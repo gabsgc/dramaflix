4ee1358c7bda4593df0b45983704ebbd4ff0aaa8
gabsgc <gabriellagarccia@gmail.com>
2020-07-27 21:02:51 -0300

    Create Project Dramaflix

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import dadosIniciais from './data/dados_iniciais.json';
+import Menu from './components/Menu';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: "#170022"}}>
+      <Menu />
+
+      <BannerMain 
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Você só vive uma vez. É sua obrigação aproveitar a vida da melhor forma possível. — Como Eu Era Antes de Você"}
+      />
+      <Carousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+      
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Footer />
     </div>
   );
 }
