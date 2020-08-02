1058bc7e6307c119d14bdad392bb5ee192abd824
gabsgc <gabriellagarccia@gmail.com>
2020-08-01 21:39:37 -0300

    validate category register form

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
@@ -38,17 +40,38 @@ function CadastroCategoria() {
 
       <form onSubmit={function handleSubmit(infosDoEvento) {
         infosDoEvento.preventDefault();
-        setCategorias([
-          ...categorias,
-          values,
-        ]);
+        if (values.titulo !== '') {
+          setCategorias([
+            ...categorias,
+            values,
+          ]);
+          toast.success('Categoria cadastrada com sucesso!', {
+            position: 'bottom-center',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        } else {
+          toast.warning('Campo(s) obrigatório(s) em branco!', {
+            position: 'bottom-center',
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          });
+        }
 
         clearForm();
       }}
       >
 
         <FormField
-          label="Título da Categoria"
+          label="*Título da Categoria"
           type="text"
           name="titulo"
           value={values.titulo}
@@ -70,10 +93,13 @@ function CadastroCategoria() {
           value={values.cor}
           onChange={handleChange}
         />
+        <h6>*Campo Obrigatório</h6>
 
         <Button type="submit">
           Cadastrar
         </Button>
+
+        <ToastContainer />
       </form>
 
       {categorias.length === 0 && (
@@ -84,14 +110,14 @@ function CadastroCategoria() {
         <h1>Categorias Registradas</h1>
         <ul>
           {categorias.map((categoria) => (
-            <li key={`${categoria.titulo}`} style={{listStyle: "none", display: 'inline', padding: '5px'}}>
+            <li key={`${categoria.titulo}`} style={{ listStyle: 'none', display: 'inline', padding: '5px' }}>
               {categoria.titulo}
             </li>
           ))}
         </ul>
       </div>
 
-      <Link to="/cadastro/video" className="linkPage" style={{padding: '10px'}}>
+      <Link to="/cadastro/video" className="linkPage" style={{ padding: '10px' }}>
         Voltar
       </Link>
       <Link to="/" className="linkPage">
